5ded839cdc25b84cfde207594154c0bc6cd633d3
Dominic Lowes <domin@Dominics-MacBook-Air.local>
2022-05-09 14:17:02 +0900

    Feat: successful load of sentiment analysis model and metadata

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const knex = require('knex');
 const port = process.env.PORT || 8080;
 require("dotenv").config({
-    path: "./.env.local",
+    path: "../.env.local",
   });
 const config = require('./knexfile');
 const app = express();
+const axios = require("axios");
 
 app.use(express.json())
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
+
 
 function App() {
+
+  const url = {
+    model: 'https://storage.googleapis.com/tfjs-models/tfjs/sentiment_lstm_v1/model.json',
+    metadata: 'https://storage.googleapis.com/tfjs-models/tfjs/sentiment_lstm_v1/metadata.json'
+};
+
+const OOV_INDEX = 2;
+
+const [metadata, setMetadata] = useState();
+const [model, setModel] = useState();
+const [testText, setText] = useState("");
+const [testScore, setScore] = useState("");
+const [trimedText, setTrim] = useState("")
+const [seqText, setSeq] = useState("")
+const [padText, setPad] = useState("")
+const [inputText, setInput] = useState("")
+const [modelLoaded, setModelLoaded] = useState(false)
+const [metadataLoaded, setMetadataLoaded] = useState(false)
+
+
+
+async function loadModel(url) {
+  try {
+    const model = await tf.loadLayersModel(url.model);
+    setModel(model);
+    setModelLoaded(true);
+  } catch {
+    console.log("model not loaded");
+  }
+}
+
+async function loadMetadata(url) {
+  try {
+    const metadataJson = await fetch(url.metadata);
+    const metadata = await metadataJson.json();
+    setMetadata(metadata);
+    setMetadataLoaded(true);
+  } catch (err) {
+    console.log("metadata not loaded");
+  }
+}
+   
+const PAD_INDEX = 0;
+
+const padSequences = (sequences, maxLen, padding = 'pre', truncating = 'pre', value = PAD_INDEX) => {
+    return sequences.map(seq => {
+      if (seq.length > maxLen) {
+        if (truncating === 'pre') {
+          seq.splice(0, seq.length - maxLen);
+        } else {
+          seq.splice(maxLen, seq.length - maxLen);
+        }
+      }
+  
+      if (seq.length < maxLen) {
+        const pad = [];
+        for (let i = 0; i < maxLen - seq.length; ++i) {
+          pad.push(value);
+        }
+        if (padding === 'pre') {
+          seq = pad.concat(seq);
+        } else {
+          seq = seq.concat(pad);
+        }
+      }
+  
+      return seq;
+    });
+  }
+  useEffect(() => {
+    if (modelLoaded === false){
+      loadModel(url);
+    }
+    if (metadataLoaded === false){
+      loadMetadata(url);
+    }
+  }, []);
+
+
+
+console.log(metadata);
+const getSentimentScore = async (text) => {
+  const inputText = text.trim().toLowerCase().replace(/(\.|\,|\!)/g, '').split(' ');
+  setTrim(inputText)
+  const sequence = inputText.map(word => {
+    let wordIndex = metadata.word_index[word] + metadata.index_from;
+    if (wordIndex > metadata.vocabulary_size) {
+      wordIndex = OOV_INDEX;
+    }
+    return wordIndex;
+  });
+  setSeq(sequence)
+  const paddedSequence = padSequences([sequence], metadata.max_len);
+  setPad(paddedSequence)
+
+  const input = tf.tensor2d(paddedSequence, [1, metadata.max_len]);
+  setInput(input)
+  const predictOut = model.predict(input);
+  const score = predictOut.dataSync()[0];
+  console.log(score);
+  predictOut.dispose();
+  setScore(score)  
+ 
+  return score;
+}
+
+useEffect(() => {
+  if (metadataLoaded === true && modelLoaded === true){
+  getSentimentScore();
+  }
+}, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Suggestion Bot
       </header>
+      <main>
+        <div className='user-input'>
+          <form>
+            <input type='text'></input>
+          </form>
+        <button>submit</button>
+        </div>
+       
+      </main>
     </div>
   );
 }
